2655f950dec0ed8d2c8778d090e602b3e887c681
Paulo19961944 <106405308+Paulo19961944@users.noreply.github.com>
2023-07-10 19:56:42 -0700

    Update calcular_on_grid.js

--- js/calcular_on_grid.js
+++ js/calcular_on_grid.js
@@ -1,109 +1,76 @@
-const calcularBtn = document.getElementById('btn');
-calcularBtn.addEventListener("click", Calcular);
+document.addEventListener("DOMContentLoaded", function() {
+    let calcularBtn = document.querySelector("#btn");
+    calcularBtn.addEventListener("click", calcular);
+});
 
-function Calcular() {
-  // Captura Dados de Entrada
-  const EnergiaMediaMensal = parseFloat(document.getElementById('p').value.replace(",", "."));
-  const EnergiaDiaria = parseFloat(document.getElementById('ed').value);
-  const HSP = parseFloat(document.getElementById('hsp').value.replace(",", "."));
-  const TensaoSistema = parseFloat(document.getElementById('v').value.replace(",", "."));
-  const TensaoBateria = parseFloat(document.getElementById('bateria').value.replace(",", "."));
-  const CapacidadeSistema = parseFloat(document.getElementById('Ah').value.replace(",", "."))
-  const TensaoModulo = parseFloat(document.getElementById('vm').value.replace(",", "."));
-  const Temperatura = parseFloat(document.getElementById('temp').value.replace(",", "."));
-  const IMP = parseFloat(document.getElementById('imp').value.replace(",", "."));
-  const ISC = parseFloat(document.getElementById('isc').value.replace(",", "."));
-  const PMAX = parseFloat(document.getElementById('pmax').value.replace(",", "."));
-  const SemMPPT = document.getElementById('NOMPPT').checked;
+// Função para calcular os valores
+function calcular() {
 
-  // Faz o Cálculo
-  let PotInversorMin = EnergiaMediaMensal / 0.7;
-  let ResultadoPotInversorMin = Math.ceil(PotInversorMin)
-  let PotInversorMax = EnergiaMediaMensal / 0.5;
-  let ResultadoPotInversorMax = Math.floor(PotInversorMax)
-  let EficienciaInversor = EnergiaDiaria / 0.9;
-  let EnergiaReal = EficienciaInversor / 0.89;
-  let CapacidadeBanco = (EnergiaReal * 3) / TensaoSistema;
-  let CapacidadeReal = CapacidadeBanco / 0.6;
-  let BateriasSerie = TensaoSistema / TensaoBateria;
-  let ResultadoBateriasSerie = Math.ceil(BateriasSerie)
-  let BateriaParalelo = CapacidadeReal / CapacidadeSistema;
-  let ResultadoBateriasParalelo = Math.floor(BateriaParalelo);
-  
-  // Verifica se o resultado é menor que 1 e atribui 1 caso seja
-  if (ResultadoBateriasParalelo < 1) {
-    ResultadoBateriasParalelo = 1;
-  }
-  
-  let TotalBaterias = ResultadoBateriasSerie * ResultadoBateriasParalelo;
-  let CorrentePainel = EnergiaReal / TensaoSistema;
-  let ResultadoPainel = Math.ceil(CorrentePainel)
-  let CoeficientePMax = Temperatura * (PMAX);
-  let RC = (100 - CoeficientePMax) / 100;
-  let QuantidadeModulosSerie = TensaoSistema / TensaoModulo;
-  let ResultadoModulosSerie = Math.ceil(QuantidadeModulosSerie);
-  let QuantidadeModulosParalelo = CorrentePainel / (RC * IMP * HSP);
-  let ResultadoModulosParalelo = Math.floor(QuantidadeModulosParalelo);
-  let QuantidadeModulosTotal = QuantidadeModulosParalelo * QuantidadeModulosSerie;
-  let ResultadoModulosTotal = Math.floor(QuantidadeModulosTotal);
-  let ISCTotal = ISC * QuantidadeModulosParalelo;
-  let CorrenteControlador = ISCTotal * 1.25;
-  let ResultadoControlador = Math.ceil(CorrenteControlador)
-  let CapacidadeDisjuntor = CorrenteControlador * 1.2;
-  let ResultadoDisjuntor = Math.floor(CapacidadeDisjuntor);
+    // Captura os valores dos campos de entrada e substitui vírgula por ponto
+    let num1 = Number(document.getElementById('EA').value.replace(',', '.'));
+    let num2 = Number(document.getElementById('HSP').value.replace(',', '.'));
+    let num3 = Number(document.getElementById('TM').value.replace(',', '.'));
+    let num4 = Number(document.getElementById('P').value.replace(',', '.'));
+    let num5 = Number(document.getElementById('V').value.replace(',', '.'));
+    let num6 = Number(document.getElementById('VM').value.replace(',', '.'));
+    let num7 = Number(document.getElementById('IM').value.replace(',', '.'));
+    let vmp = Number(document.getElementById('VMP').value.replace(',', '.'));
+    let voc = Number(document.getElementById('VOC').value.replace(',', '.'));
+    let isc = Number(document.getElementById('ISC').value.replace(',', '.'));
 
+    // Captura os Dados do HTML
+    let ModulosTotal = document.querySelector('#MT');
+    let ModulosSerieMax = document.querySelector('#MSMax');
+    let ModulosSerieMin = document.querySelector('#MSMin');
+    let ModulosParalelo = document.querySelector('#MP');
+    let Fusivel = document.querySelector('#Fuse');
+    let Disjuntor = document.querySelector('#CA');
 
-  // Captura os Dados
-  const ModulosTotal = document.getElementById('MT');
-  const ModulosSerie = document.getElementById('MS');
-  const ModulosParalelo = document.getElementById('MP');
-  const Disjuntor = document.getElementById('CA');
-  const PotenciaInversor = document.getElementById('Pot-Inversor');
-  const NumeroBateriasSerie = document.getElementById('BS');
-  const NumeroBateriasParalelo = document.getElementById('BP');
-  const NumeroTotalBaterias = document.getElementById('NB');
-  const CorrenteTotalPainel = document.getElementById('Corrente-Painel');
-  const CorrenteTotalControlador = document.getElementById('Corrente-Controlador');
+    // Realiza o cálculo
+    let EMM = num1 / 12;
+    let EMD = EMM / 30;
+    let Sujeira = num2 * 0.95;
+    let PotenciaPico = EMD / num2;
+    let PotenciaPicoReal = PotenciaPico / 0.967;
+    let CTPmax = num3 * 0.8934;
+    let CTVoc = voc * 0.9194;
+    let CTISC = isc * 1.0138;
+    let PmaxCorrigida = (num4 * 0.8934) / 1000;
+    let QM = PotenciaPicoReal / PmaxCorrigida;
+    let resultadoQM = Math.ceil(QM);
+    let QSmin = num5 / vmp;
+    let resultadoQSmin = Math.ceil(QSmin);
+    let QSmax = num6 / CTVoc;
+    let resultadoQSmax = Math.floor(QSmax);
+    let QP = num7 / CTISC;
+    let resultadoQP = Math.floor(QP);
+    let DJ = num7 * 1.2;
+    let resultadoDJ = Math.floor(DJ);
+    let CC = num7 * 0.9;
+    let resultadoCC = Math.floor(CC);
 
-  // Insere o Texto no HTML
-  if (!isNaN(QuantidadeModulosTotal)) {
-    ModulosTotal.textContent = "Quantidade de Módulos Total: " + ResultadoModulosTotal;
-  }
+    // Verifica se os elementos foram encontrados antes de definir o 'textContent'
+    if (!isNaN(resultadoQM)) {
+        ModulosTotal.textContent = "Quantidade de Módulos em Série: " + resultadoQM + ' ' + 'Módulo(s)';
+    }
+    if (!isNaN(resultadoQSmax)) {
+        ModulosSerieMax.textContent = "Quantidade Máxima de Módulos em Série: " + resultadoQSmax + ' ' + 'Módulo(s)';
+    }
+    if (!isNaN(resultadoQSmin)) {
+        ModulosSerieMin.textContent = "Quantidade Mínima de Módulos em Série: " + resultadoQSmin + ' ' + 'Módulo(s)';
+    }
+    if (!isNaN(resultadoQP)) {
+        ModulosParalelo.textContent = "Quantidade de Módulos em Paralelo: " + resultadoQP + ' ' + 'Módulo(s)';
+    }
+    if (!isNaN(resultadoCC)) {
+        Fusivel.textContent = "Fusível (CC): " + resultadoCC + ' ' + '(A)';
+    }
+    if (!isNaN(resultadoDJ)) {
+        Disjuntor.textContent = "Disjuntor (CA): " + resultadoDJ + ' ' + '(A)';
+    } 
 
-  if (!isNaN(QuantidadeModulosSerie)) {
-    ModulosSerie.textContent = "Quantidade de Módulos em Série: " + ResultadoModulosSerie;
-  }
-
-  if (!isNaN(QuantidadeModulosParalelo)) {
-    ModulosParalelo.textContent = "Quantidade de Módulos em Paralelo: " + ResultadoModulosParalelo;
-  }
-
-  if (!isNaN(CapacidadeDisjuntor)) {
-    Disjuntor.textContent = "Disjuntor (A): " + ResultadoDisjuntor;
-  }
-
-  if (!isNaN(PotInversorMin) && !isNaN(PotInversorMax)) {
-    PotenciaInversor.textContent = "A Potência do Inversor deve ser entre: " + ResultadoPotInversorMin + " e " + ResultadoPotInversorMax;
-  }
-
-
-  if (!isNaN(BateriasSerie)) {
-    NumeroBateriasSerie.textContent = "A Quantidade de Baterias em Série é de: " + ResultadoBateriasSerie;
-  }
-
-  if (!isNaN(BateriaParalelo)) {
-    NumeroBateriasParalelo.textContent = "A Quantidade de Baterias em Paralelo é de: " + ResultadoBateriasParalelo;
-  }
-
-  if (!isNaN(TotalBaterias)) {
-    NumeroTotalBaterias.textContent = "A Quantidade Total de Baterias é de: " + TotalBaterias;
-  }
-
-  if (!isNaN(CorrentePainel)) {
-    CorrenteTotalPainel.textContent = "A Corrente Total do Painel é de: " + ResultadoPainel;
-  }
-
-  if (!isNaN(CorrenteControlador)) {
-    CorrenteTotalControlador.textContent = "A Corrente do Controlador é: " + ResultadoControlador;
-  }
+    if (isNaN(resultadoQM) || isNaN(resultadoQSmax) || isNaN(resultadoQSmin) || isNaN(resultadoQP) || isNaN(resultadoCC) || isNaN(resultadoDJ)) {
+        alert("Preencha todos os campos corretamente");
+    }
+   
 }
